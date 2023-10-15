ee9c89970aa30fd6c98c196503b33100f8aab57f
= <=>
2023-10-15 22:04:44 +0500

    task is completed

---
+++ index.js
@@ -0,0 +1,24 @@
+// 1
+let name = prompt("Enter your name")
+if (name.length % 2 === 0) {
+    alert("Welcome, " + name + "!")
+  } else {
+    alert("Please leave the page "+ name)
+  }
+
+// 2
+let number = prompt("Please enter a number")
+if (number % 2 ===0) {
+    alert("That is the even number")
+} else {
+    alert("That is the odd number")
+}
+
+// 3
+let name_two = prompt("Enter your name")
+if (name_two.charAt(0).toLowerCase() === "a") {
+    alert("Welcome, " + name_two + "!");
+  } else {
+    alert("Please leave the page "+ name_two);
+  }
+  
